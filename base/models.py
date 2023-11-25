ad9b1338013b3b337fa9ede55f675aa14891f22d
yash16jr <yash16jr@gmail.com>
2023-11-25 14:28:01 +0530

    topic-model

--- base/models.py
+++ base/models.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
-    #host = 
-    #topic = 
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null = True) 
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null = True) 
     name = models.CharField(max_length = 200)
     decription = models.TextField(null = True, blank = True)
     #participants = 
@@ -25,4 +32,4 @@ class Message(models.Model):
     created = models.DateTimeField(auto_now_add = True)
     
     def __str__(self):
-        return self.bosy[0:50]
+        return self.body[0:50]
