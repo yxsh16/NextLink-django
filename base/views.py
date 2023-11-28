4dd3846e1b9518ee1a4155c03467e8a78e807542
yash16jr <yash16jr@gmail.com>
2023-11-28 13:29:48 +0530

    user_registration

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout 
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
@@ -11,11 +12,12 @@ from django.db.models import Q
 
 
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -29,13 +31,31 @@ def loginPage(request):
         else:
             messages.error(request,"Username or password incorrect")
                      
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occurred during registration")
+    
+    return render(request, 'base/login_register.html', {'form':form})
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -54,6 +74,10 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id = pk)
+    room_message = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    
+    
     context = {'room': room}        
     return render(request, 'base/room.html', context)
 
