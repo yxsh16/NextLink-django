ce1a79be4cc031a746ad9b540c4b87ebf23318fd
yash16jr <yash16jr@gmail.com>
2023-11-28 21:19:25 +0530

    chat_CRUD

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout 
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
@@ -74,11 +74,18 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id = pk)
-    room_message = room.message_set.all().order_by('-created')
-    participants = room.participants.all()
+    room_messages = room.message_set.all().order_by('-created')
+    # participants = room.participants.all()
     
+    if request.method == 'POST':
+            message = Message.objects.create(
+                user = request.user,
+                room = room, 
+                body = request.POST.get('body')   
+            )
+            return redirect('room', pk = room.id)
     
-    context = {'room': room}        
+    context = {'room': room, 'room_messages' : room_messages}        
     return render(request, 'base/room.html', context)
 
 
