742cc4530e859339e74d545230be9039a0817028
yash16jr <yash16jr@gmail.com>
2023-11-28 22:15:14 +0530

    msg_CRUD

--- base/views.py
+++ base/views.py
@@ -75,7 +75,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id = pk)
     room_messages = room.message_set.all().order_by('-created')
-    # participants = room.participants.all()
+    participants = room.participants.all()
     
     if request.method == 'POST':
             message = Message.objects.create(
@@ -83,9 +83,10 @@ def room(request, pk):
                 room = room, 
                 body = request.POST.get('body')   
             )
+            room.participants.add(request.user)
             return redirect('room', pk = room.id)
     
-    context = {'room': room, 'room_messages' : room_messages}        
+    context = {'room': room, 'room_messages' : room_messages, 'participants': participants}        
     return render(request, 'base/room.html', context)
 
 
@@ -131,3 +132,16 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/room_delete.html',{'obj':room})
+
+
+@login_required(login_url='/login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id = pk)
+    
+    if request.user != message.user:
+        return HttpResponse("You are not allowed here!!!")
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/room_delete.html',{'obj':message})
