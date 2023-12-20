dcf37ff3f0ba2d5b14ecb4de8abdeaed8e65122c
yash16jr <yash16jr@gmail.com>
2023-12-20 15:45:31 +0530

    custom-usermodel

--- base/views.py
+++ base/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout 
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
--- gamers_hub/settings.py
+++ gamers_hub/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "corsheaders",
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
