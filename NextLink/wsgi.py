6a36e27c7a65e97d830ded75d8e6fc2f4c315b05
yash16jr <yash16jr@gmail.com>
2023-12-23 18:56:50 +0530

    update

--- NextLink/wsgi.py
+++ NextLink/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gamers_hub project.
+WSGI config for NextLink project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamers_hub.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NextLink.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamers_hub.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NextLink.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
